c15727269145f807d5ba5c79ccfb5dd417ec858f
orion <nnmm5122004@gmail.com>
2026-02-21 00:57:13 +0630

    :lock::sparkles:jwt config

--- auth-service/src/main/java/com/orion/auth_service/controller/PingController.java
+++ auth-service/src/main/java/com/orion/auth_service/controller/PingController.java
@@ -1,16 +1,34 @@
 package com.orion.auth_service.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("${api.base.path}/v1/ping")
 public class PingController {
+
     @GetMapping
-    public Map<String,String> pingServer(){
-        return Map.of("msg","server is running at 8080");
+    public Map<String,String> pingServer(@RequestHeader(value = HttpHeaders.USER_AGENT, required = false) String userAgent){
+        Map<String,String > map = new HashMap<>();
+        map.put("msg", "server is running at 8081");
+        map.put("user-agent" , userAgent);
+        return map;
     }
+
+    public String getClientPublicIp(HttpServletRequest request) {
+        String xForwardedFor = request.getHeader("X-Forwarded-For");
+        if (xForwardedFor != null && !xForwardedFor.isEmpty()) {
+            // The first IP in the list is the original client's public IP
+            return xForwardedFor.split(",")[0].trim();
+        }
+        return request.getRemoteAddr(); // Fallback if no proxy is used
+    }
+
 }
